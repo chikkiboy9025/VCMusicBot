49f5151fdae5c767afc4fd851d3d7be4e9a3868e
▄︻デƈօɮʀǟ ɢǟռɢ═══━一 <editingpro884@gmail.com>
2025-06-20 22:59:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pytgcalls import PyTgCalls
 from pytgcalls.types.input_stream import InputAudioStream
 import yt_dlp
 
-api_id = 12345678  # Replace with your API ID
-api_hash = "your_api_hash_here"  # Replace with your API HASH
-session = "your_string_session_here"  # Replace with your String Session
+api_id = 20955666  # Replace with your API ID
+api_hash = "a5eba0f71ecf9340a52f4a41dfc89e3f"  # Replace with your API HASH
+session = "BQE_whIAxYjjMmzzQC8JNVFefDIR-PUS8Cak4a4EqmgQ_htG6QIuw1zixeeiYfr7vnZ5Au1F6635RU8U67J_QJa_tXIwo5_pVZmf4FtwJLMO6rva9aR4dh7V467r8MDcqxoyNKJ3g_eErpKOl_WsV5ctnUxbweqnwBO5OjF9VO7Te4TJJEfpU55E-R25Kcq80Vzy9uS3Z7HZ9PPL6K87fP2rMGGk1iTI1JLX3seZ-takrvT0-pD1g--GMK_hJf0SJyvza2LvKg7gqr4NuA2mPT6F29PLHddj15DPqDn74-MACgMDDJaT5ExLL1oLaT0gWGrK8-CE4pJCzbRpaW1yq-FIrl2hcwAAAAGyPxo3AA"  # Replace with your String Session
 
 app = Client(session, api_id=api_id, api_hash=api_hash)
 vc = PyTgCalls(app)
